5e2851603ca5111fdb56cdd65f53ad2cf9081df1
Salmann107 <salman_almash3oo@outlook.com>
2022-10-16 10:16:51 +0300

    dyij

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
-
+const ejslayouts = require('express-ejs-layouts')
 //MIDDELWARE
 app.set('view engine','ejs')
+app.use(ejslayouts)
 
 //ROUTS
+//home
+app.get('/', (req, res) => {
+    res.render('home.ejs')
+})
+//favourite animals
+app.get('/animals', (req,res) => {
+    res.render('animals.ejs', {animals: ['sand crab', 'corny joke', 'bendicat the sea cucumber']
+})
+})
 
 app.listen(PORT, () => {
     console.log('You are running the app')
